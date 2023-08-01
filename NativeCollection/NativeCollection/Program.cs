de8308acbe9d4580b4cea49aca6737c57cb1eae9
wuwenchao <susices@hotmail.com>
2023-08-01 15:23:10 +0800

    创建项目

---
+++ NativeCollection/NativeCollection/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace NativeCollection;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello");
+    }
+}
